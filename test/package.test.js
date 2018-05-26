1c55725154ad0157f77e329518ced521b0ff3997
Jared Hanson <jaredhanson@gmail.com>
2018-05-26 16:34:22 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+var pkg = require('..');
+var expect = require('chai').expect;
+
+
+describe('tokens-paseto', function() {
+  
+  it('should export functions', function() {
+    expect(pkg.seal).to.be.a('function');
+    expect(pkg.unseal).to.be.a('function')
+  });
+  
+});
---
+++ test/seal.test.js
@@ -0,0 +1,35 @@
+/* global describe, it */
+
+var Paseto = require('paseto.js');
+var setup = require('../lib/seal');
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+
+describe('seal', function() {
+  
+  describe('defaults', function() {
+    
+    describe('encrypting to self', function() {
+      var token;
+      
+      var keying = sinon.stub().yields(null, { secret: '12abcdef7890abcdef7890abcdef7890' });
+      
+      before(function(done) {
+        var seal = setup(keying);
+        seal({ foo: 'bar' }, { identifier: 'https://self-issued.me' }, function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      it('should generate a token', function() {
+        expect(token.length).to.be.above(0);
+        expect(token.substr(0, 9)).to.equal('v1.local.');
+      });
+      
+    });
+    
+  });
+  
+});

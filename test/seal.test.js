074b95d974010886e4c5eebb75b82db3464e0339
Jared Hanson <jaredhanson@gmail.com>
2018-05-26 17:54:06 -0700

    Stub in version negotiation.

--- test/seal.test.js
+++ test/seal.test.js
@@ -10,7 +10,7 @@ describe('seal', function() {
   
   describe('defaults', function() {
     
-    describe('encrypting to self', function() {
+    describe.only('encrypting to recipient using defaults', function() {
       var token;
       
       var keying = sinon.stub().yields(null, { secret: '12abcdef7890abcdef7890abcdef7890' });
@@ -28,7 +28,34 @@ describe('seal', function() {
         expect(token.substr(0, 9)).to.equal('v2.local.');
       });
       
-    });
+    }); // encrypting to recipient using defaults
+    
+    describe('encrypting to recipient, negotiating v1', function() {
+      var token;
+      
+      var keying = sinon.stub().yields(null, { secret: '12abcdef7890abcdef7890abcdef7890' });
+      
+      before(function(done) {
+        var recipient = {
+          id: 'https://api.example.com/'
+        };
+        var options = {
+          versions: [ 'v1' ]
+        }
+        
+        var seal = setup(keying);
+        seal({ foo: 'bar' }, recipient, function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      it('should generate a token', function() {
+        expect(token.length).to.be.above(0);
+        expect(token.substr(0, 9)).to.equal('v2.local.');
+      });
+      
+    }); // encrypting to recipient, negotiating v1
     
   });
   
